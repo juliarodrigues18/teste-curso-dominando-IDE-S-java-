79427bd5c0f3d4d35bd450712374a9e7a36fef7b
Julia Rodrigues <julia.uber@gmail.com>
2022-02-23 13:51:24 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+       /* int a = 5;
+        int b = 3;
+
+        System.out.println("Hello Word!"+ (a + b));
+*/
+    }
+
+}
